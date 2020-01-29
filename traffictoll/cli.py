8d66ebd57eb25141bdf2318f610ca2cd33233220
cryzed <cryzed@googlemail.com>
2020-01-29 23:26:31 +0100

    Use _TrafficType enum instead of strings

--- traffictoll/cli.py
+++ traffictoll/cli.py
@@ -1,6 +1,7 @@
 import argparse
 import atexit
 import collections
+import enum
 import time
 from typing import Dict, DefaultDict, Set
 
@@ -18,6 +19,11 @@ from .tc import (
 )
 
 CONFIG_ENCODING = "UTF-8"
+
+
+class _TrafficType(enum.Enum):
+    Ingress = enum.auto()
+    Egress = enum.auto()
 
 
 def get_argument_parser() -> argparse.ArgumentParser:
@@ -66,7 +72,10 @@ def main(arguments: argparse.Namespace) -> None:
     atexit.register(_clean_up, ingress_interface, egress_interface)
 
     process_filter_predicates = []
-    class_ids: Dict[str, Dict[str, int]] = {"ingress": {}, "egress": {}}
+    class_ids: Dict[_TrafficType, Dict[str, int]] = {
+        _TrafficType.Ingress: {},
+        _TrafficType.Egress: {},
+    }
     for name, process in (config.get("processes", {}) or {}).items():
         # Prepare process filter predicates to match network connections
         conditions = [list(match.items())[0] for match in process.get("match", [])]
@@ -93,7 +102,7 @@ def main(arguments: argparse.Namespace) -> None:
                 ingress_root_class_id,
                 download_rate,
             )
-            class_ids["ingress"][name] = egress_class_id
+            class_ids[_TrafficType.Ingress][name] = egress_class_id
         if upload_rate:
             logger.info(
                 "Setting up upload limiter for {!r} with max rate {}", name, upload_rate
@@ -101,9 +110,12 @@ def main(arguments: argparse.Namespace) -> None:
             ingress_class_id = tc_add_htb_class(
                 egress_interface, egress_qdisc_id, egress_root_class_id, upload_rate
             )
-            class_ids["egress"][name] = ingress_class_id
+            class_ids[_TrafficType.Egress][name] = ingress_class_id
 
-    port_to_filter_id: Dict[str, Dict[int, str]] = {"ingress": {}, "egress": {}}
+    port_to_filter_id: Dict[_TrafficType, Dict[int, str]] = {
+        _TrafficType.Ingress: {},
+        _TrafficType.Egress: {},
+    }
 
     def add_ingress_filter(port: int, class_id: int) -> None:
         filter_id = tc_add_u32_filter(
@@ -112,32 +124,32 @@ def main(arguments: argparse.Namespace) -> None:
             ingress_qdisc_id,
             class_id,
         )
-        port_to_filter_id["ingress"][port] = filter_id
+        port_to_filter_id[_TrafficType.Ingress][port] = filter_id
 
     def add_egress_filter(port: int, class_id: int) -> None:
         filter_id = tc_add_u32_filter(
             egress_interface, f"match ip sport {port} 0xffff", egress_qdisc_id, class_id
         )
-        port_to_filter_id["egress"][port] = filter_id
+        port_to_filter_id[_TrafficType.Egress][port] = filter_id
 
     def remove_filters(port: int) -> None:
-        ingress_filter_id = port_to_filter_id["ingress"].get(port)
+        ingress_filter_id = port_to_filter_id[_TrafficType.Ingress].get(port)
         if ingress_filter_id:
             tc_remove_u32_filter(ingress_interface, ingress_filter_id, ingress_qdisc_id)
-            del port_to_filter_id["ingress"][port]
+            del port_to_filter_id[_TrafficType.Ingress][port]
 
-        egress_filter_id = port_to_filter_id["egress"].get(port)
+        egress_filter_id = port_to_filter_id[_TrafficType.Egress].get(port)
         if egress_filter_id:
             tc_remove_u32_filter(egress_interface, egress_filter_id, egress_qdisc_id)
-            del port_to_filter_id["egress"][port]
+            del port_to_filter_id[_TrafficType.Egress][port]
 
     filtered_ports: DefaultDict[str, Set[int]] = collections.defaultdict(set)
     while True:
         filtered_connections = filter_net_connections(process_filter_predicates)
         for name, connections in filtered_connections.items():
             ports = set(connection.laddr.port for connection in connections)
-            ingress_class_id = class_ids["ingress"].get(name)
-            egress_class_id = class_ids["egress"].get(name)
+            ingress_class_id = class_ids[_TrafficType.Ingress].get(name)
+            egress_class_id = class_ids[_TrafficType.Egress].get(name)
 
             # Add new port filters
             new_ports = sorted(ports.difference(filtered_ports[name]))
